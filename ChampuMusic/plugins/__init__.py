0dbbc19368dace36d5977d3055bba863a10aacd0
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-07-16 23:38:59 +0700

    Update __init__.py

--- ChampuMusic/plugins/__init__.py
+++ ChampuMusic/plugins/__init__.py
@@ -23,7 +23,7 @@ ROOT_DIR = abspath(join(dirname(__file__), "..", ".."))
 
 EXTERNAL_REPO_PATH = join(ROOT_DIR, EXTRA_PLUGINS_FOLDER)
 
-extra_plugins_enabled = EXTRA_PLUGINS.lower() == "false"
+extra_plugins_enabled = EXTRA_PLUGINS.lower() == "False"
 
 if extra_plugins_enabled:
     if not os.path.exists(EXTERNAL_REPO_PATH):
