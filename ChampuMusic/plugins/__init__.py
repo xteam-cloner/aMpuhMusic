97849ef3bb5c2d380c1dc01e5a0ca12bbe010d3e
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-07-16 23:44:13 +0700

    Update __init__.py

--- ChampuMusic/plugins/__init__.py
+++ ChampuMusic/plugins/__init__.py
@@ -7,7 +7,7 @@ import subprocess
 import sys
 from os.path import abspath, dirname, isfile, join
 
-from config import EXTRA_PLUGINS, EXTRA_PLUGINS_FOLDER, EXTRA_PLUGINS_REPO
+#from config import EXTRA_PLUGINS, EXTRA_PLUGINS_FOLDER, EXTRA_PLUGINS_REPO
 from ChampuMusic import LOGGER
 
 logger = LOGGER(__name__)
